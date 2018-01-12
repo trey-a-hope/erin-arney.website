e7eaa28b77377b14c4578b0596672acb59f7839d
Trey Hope <treyhope@Treys-MacBook-Air.local>
2018-01-11 22:50:59 -0500

    Added Blog Service. Added simple content to the rest of the pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
+import { HttpModule }                   from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+//Views
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { NavbarComponent } from './views/navbar/navbar.component';
 import { FooterComponent } from './views/footer/footer.component';
-import { RoutingModule } from './/routing.module';
 import { ProjectsComponent } from './views/projects/projects.component';
 import { BlogComponent } from './views/blog/blog.component';
 import { ContactComponent } from './views/contact/contact.component';
 
+//Routing
+import { RoutingModule } from './/routing.module';
+
+//Services
+import { BlogService }                  from './services/blog.service';
 
 @NgModule({
   declarations: [
@@ -29,9 +36,14 @@ import { ContactComponent } from './views/contact/contact.component';
     BrowserModule,
     RoutingModule,
     NgbModule.forRoot(),
-    Angular2FontawesomeModule
+    Angular2FontawesomeModule,
+    HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    BlogService
+  ],
+  bootstrap: [ 
+    AppComponent 
+  ]
 })
 export class AppModule { }
--- src/app/views/blog/blog.component.ts
+++ src/app/views/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService }       from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  posts: [any];
 
-  constructor() { }
+  constructor(private blogService: BlogService) { }
 
   ngOnInit() {
+    this.getBlogPosts();
+  }
+
+  private getBlogPosts(): void {
+    this.blogService.getPosts().subscribe(data => {
+      this.posts = data;
+    });
+  }
+
+  public share(provider: string, blogUrl: string): void {
+    var text: string = 'Check out this blog I found on td5.tr3umphant-designs.com!';
+
+    switch (provider) {
+        case 'TWITTER':
+            window.open('http://twitter.com/share?url='+encodeURIComponent(blogUrl)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        case 'FACEBOOK':
+            window.open('http://facebook.com/sharer/sharer.php?u='+encodeURIComponent(blogUrl)+'&title='+encodeURIComponent(text), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        case 'LINKEDIN':
+            window.open('http://www.linkedin.com/shareArticle?mini=true&url='+encodeURIComponent(blogUrl)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        case 'GOOGLE+':
+            window.open('https://plus.google.com/share?url='+encodeURIComponent(blogUrl)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        default:
+            break;
+    }
   }
 
 }
